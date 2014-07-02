003fe9c5ef514b1f2d8d986d587d046fce6c3eb2
Barry vd. Heuvel <barryvdh@gmail.com>
2014-07-02 20:58:44 +0200

    Tweak ServiceProvider
    Pull the defines etc outside the dompdf binding. Will only be run when
    dompdf is requested, because of deferred.

--- src/Barryvdh/DomPDF/ServiceProvider.php
+++ src/Barryvdh/DomPDF/ServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Barryvdh\DomPDF;
 
+use Exception;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
 class ServiceProvider extends IlluminateServiceProvider {
@@ -22,37 +23,36 @@ class ServiceProvider extends IlluminateServiceProvider {
 		$this->app['config']->package('barryvdh/laravel-dompdf', __DIR__ . '/../../config');
 	}
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
+    /**
+     * Register the service provider.
+     *
+     * @throws \Exception
+     * @return void
+     */
 	public function register()
 	{
-        $this->app['dompdf'] = $this->app->share(function($app)
+
+        $defines = $this->app['config']->get('laravel-dompdf::defines') ?: array();
+        foreach($defines as $key => $value){
+            $this->define($key, $value);
+        }
+
+        //Still load these values, in case config is not used.
+        $this->define("DOMPDF_ENABLE_REMOTE", true);
+        $this->define("DOMPDF_ENABLE_AUTOLOAD", false);
+        $this->define("DOMPDF_CHROOT",  $this->app['path.base']);
+        $this->define("DOMPDF_LOG_OUTPUT_FILE", $this->app['path.storage'] . '/logs/dompdf.html');
+
+        $config_file = $this->app['config']->get('laravel-dompdf::config_file') ?:  $this->app['path.base'] .'/vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+        if(file_exists($config_file)){
+            require_once $config_file;
+        }else{
+            throw new Exception("$config_file cannot be loaded, please configure correct config file (config.php: config_file)");
+        }
+
+        $this->app->bind('dompdf', function($app)
             {
-
-                $basePath = $app['path.base'];
-                $defines = $app['config']->get('laravel-dompdf::defines') ?: array();
-                foreach($defines as $key => $value){
-                    $this->define($key, $value);
-                }
-
-                //Still load these values, in case config is not used.
-                $this->define("DOMPDF_ENABLE_REMOTE", true);
-                $this->define("DOMPDF_ENABLE_AUTOLOAD", false);
-                $this->define("DOMPDF_CHROOT", $basePath);
-                $this->define("DOMPDF_LOG_OUTPUT_FILE", $app['path.storage'] . '/logs/dompdf.html');
-
-
-                $config_file = $app['config']->get('laravel-dompdf::config_file') ?: $basePath .'/vendor/dompdf/dompdf/dompdf_config.inc.php';
-
-                if(file_exists($config_file)){
-                    require_once $config_file;
-                }else{
-                    throw new Exception("$config_file cannot be loaded, please configure correct config file (config.php: config_file)");
-                }
-
                 return new PDF($app['config'], $app['files'], $app['view'], $app['path.public']);
             });
 	}
