c037c9f2d955bd3812eadcb368b37167b003050f
Barry vd. Heuvel <barryvdh@gmail.com>
2014-08-13 19:20:52 +0200

    Fix ServiceProvider order
    Make sure the config isn't read before the namespace is defined.. Fixes #61

--- src/Barryvdh/DomPDF/ServiceProvider.php
+++ src/Barryvdh/DomPDF/ServiceProvider.php
@@ -6,22 +6,44 @@ use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
 class ServiceProvider extends IlluminateServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->app['config']->package('barryvdh/laravel-dompdf', __DIR__ . '/../../config');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     */
+    public function boot()
+    {
+        $this->app['config']->package('barryvdh/laravel-dompdf', __DIR__ . '/../../config');
+
+        $defines = $this->app['config']->get('laravel-dompdf::defines') ?: array();
+        foreach ($defines as $key => $value) {
+            $this->define($key, $value);
+        }
+
+        //Still load these values, in case config is not used.
+        $this->define("DOMPDF_ENABLE_REMOTE", true);
+        $this->define("DOMPDF_ENABLE_AUTOLOAD", false);
+        $this->define("DOMPDF_CHROOT", $this->app['path.base']);
+        $this->define("DOMPDF_LOG_OUTPUT_FILE", $this->app['path.storage'] . '/logs/dompdf.html');
+
+        $config_file = $this->app['config']->get(
+            'laravel-dompdf::config_file'
+        ) ?: $this->app['path.base'] . '/vendor/dompdf/dompdf/dompdf_config.inc.php';
+
+        if (file_exists($config_file)) {
+            require_once $config_file;
+        } else {
+            throw new Exception(
+                "$config_file cannot be loaded, please configure correct config file (config.php: config_file)"
+            );
+        }
+    }
 
     /**
      * Register the service provider.
@@ -29,46 +51,26 @@ class ServiceProvider extends IlluminateServiceProvider {
      * @throws \Exception
      * @return void
      */
-	public function register()
-	{
-
-        $defines = $this->app['config']->get('laravel-dompdf::defines') ?: array();
-        foreach($defines as $key => $value){
-            $this->define($key, $value);
-        }
-
-        //Still load these values, in case config is not used.
-        $this->define("DOMPDF_ENABLE_REMOTE", true);
-        $this->define("DOMPDF_ENABLE_AUTOLOAD", false);
-        $this->define("DOMPDF_CHROOT",  $this->app['path.base']);
-        $this->define("DOMPDF_LOG_OUTPUT_FILE", $this->app['path.storage'] . '/logs/dompdf.html');
-
-        $config_file = $this->app['config']->get('laravel-dompdf::config_file') ?:  $this->app['path.base'] .'/vendor/dompdf/dompdf/dompdf_config.inc.php';
-
-        if(file_exists($config_file)){
-            require_once $config_file;
-        }else{
-            throw new Exception("$config_file cannot be loaded, please configure correct config file (config.php: config_file)");
-        }
-
-        $this->app->bind('dompdf', function($app)
-            {
+    public function register()
+    {
+        $this->app->bind('dompdf', function ($app) {
                 return new PDF($app['config'], $app['files'], $app['view'], $app['path.public']);
             });
-	}
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('dompdf');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('dompdf');
+    }
 
-    protected function define($name, $value){
-        if ( !defined($name) ) {
+    protected function define($name, $value)
+    {
+        if (!defined($name)) {
             define($name, $value);
         }
     }
