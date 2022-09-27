6de09bd1f00d8d648a38800446a4d571ead7871a
Michael Rubél <contact@observer.name>
2022-09-27 10:34:33 +0200

    remove redundant static call

--- src/Facade/Pdf.php
+++ src/Facade/Pdf.php
@@ -34,39 +34,4 @@ class Pdf extends IlluminateFacade
     {
         return 'dompdf.wrapper';
     }
-
-    /**
-     * Resolve a new instance
-     * @param string $method
-     * @param array<mixed> $args
-     * @return mixed
-     */
-    public static function __callStatic($method, $args)
-    {
-        $instance = static::$app->make(static::getFacadeAccessor());
-
-        switch (count($args)) {
-            case 0:
-                return $instance->$method();
-
-            case 1:
-                return $instance->$method($args[0]);
-
-            case 2:
-                return $instance->$method($args[0], $args[1]);
-
-            case 3:
-                return $instance->$method($args[0], $args[1], $args[2]);
-
-            case 4:
-                return $instance->$method($args[0], $args[1], $args[2], $args[3]);
-
-            default:
-                $callable = [$instance, $method];
-                if (! is_callable($callable)) {
-                    throw new \UnexpectedValueException("Method PDF::{$method}() does not exist.");
-                }
-                return call_user_func_array($callable, $args);
-        }
-    }
 }
