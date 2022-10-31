5985e7ac8a6611b5afd7e8bf2d9077bbce19f3ab
erikn69 <erikn_69@hotmail.com>
2022-10-31 09:03:13 -0500

    Support custom disks saving (#912)

--- src/PDF.php
+++ src/PDF.php
@@ -10,6 +10,7 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Contracts\View\Factory as ViewFactory;
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * A Laravel wrapper for Dompdf
@@ -174,8 +175,15 @@ class PDF
     /**
      * Save the PDF to a file
      */
-    public function save(string $filename): self
+    public function save(string $filename, string $disk = null): self
     {
+        $disk = $disk ?: $this->config->get('dompdf.disk');
+
+        if (! is_null($disk)) {
+            Storage::disk($disk)->put($filename, $this->output());
+            return $this;
+        }
+
         $this->files->put($filename, $this->output());
         return $this;
     }
