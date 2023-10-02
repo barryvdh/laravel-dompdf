90e680feb4aa7109c9a1288be2508af77d6f80e3
erikn69 <erikn_69@hotmail.com>
2023-10-02 02:14:07 -0500

    Fix phpstan analysis (#972)

--- src/Facade/Pdf.php
+++ src/Facade/Pdf.php
@@ -2,26 +2,33 @@
 
 namespace Barryvdh\DomPDF\Facade;
 
+use Barryvdh\DomPDF\PDF as BasePDF;
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
 /**
- * @method static \Barryvdh\DomPDF\PDF setPaper($paper, $orientation = 'portrait')
- * @method static \Barryvdh\DomPDF\PDF setBaseHost(string $baseHost)
- * @method static \Barryvdh\DomPDF\PDF setProtocol(string $protocol)
- * @method static \Barryvdh\DomPDF\PDF setHttpContext($httpContext)
- * @method static \Barryvdh\DomPDF\PDF setCallbacks(array $callbacks)
- * @method static \Barryvdh\DomPDF\PDF setWarnings($warnings)
- * @method static \Barryvdh\DomPDF\PDF setOption(array|string $attribute, $value = null)
- * @method static \Barryvdh\DomPDF\PDF setOptions(array $options)
- * @method static \Barryvdh\DomPDF\PDF loadView($view, $data = array(), $mergeData = array(), $encoding = null)
- * @method static \Barryvdh\DomPDF\PDF loadHTML($string, $encoding = null)
- * @method static \Barryvdh\DomPDF\PDF loadFile($file)
- * @method static \Barryvdh\DomPDF\PDF addInfo($info)
- * @method static string output($options = [])
- * @method static \Barryvdh\DomPDF\PDF save()
- * @method static \Illuminate\Http\Response download($filename = 'document.pdf')
- * @method static \Illuminate\Http\Response stream($filename = 'document.pdf')
- *
+ * @method static BasePDF setBaseHost(string $baseHost)
+ * @method static BasePDF setBasePath(string $basePath)
+ * @method static BasePDF setCanvas(\Dompdf\Canvas $canvas)
+ * @method static BasePDF setCallbacks(array $callbacks)
+ * @method static BasePDF setCss(\Dompdf\Css\Stylesheet $css)
+ * @method static BasePDF setDefaultView(string $defaultView, array $options)
+ * @method static BasePDF setDom(\DOMDocument $dom)
+ * @method static BasePDF setFontMetrics(\Dompdf\FontMetrics $fontMetrics)
+ * @method static BasePDF setHttpContext(resource|array $httpContext)
+ * @method static BasePDF setPaper(string|float[] $paper, string $orientation = 'portrait')
+ * @method static BasePDF setProtocol(string $protocol)
+ * @method static BasePDF setTree(\Dompdf\Frame\FrameTree $tree)
+ * @method static BasePDF setWarnings(bool $warnings)
+ * @method static BasePDF setOption(array|string $attribute, $value = null)
+ * @method static BasePDF setOptions(array $options)
+ * @method static BasePDF loadView(string $view, array $data = [], array $mergeData = [], ?string $encoding = null)
+ * @method static BasePDF loadHTML(string $string, ?string $encoding = null)
+ * @method static BasePDF loadFile(string $file)
+ * @method static BasePDF addInfo(array $info)
+ * @method static string output(array $options = [])
+ * @method static BasePDF save()
+ * @method static \Illuminate\Http\Response download(string $filename = 'document.pdf')
+ * @method static \Illuminate\Http\Response stream(string $filename = 'document.pdf')
  */
 class Pdf extends IlluminateFacade
 {
